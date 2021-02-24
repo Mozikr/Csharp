5f2d500976b422329920148244a36b352fa8393b
Szymon <52745180+Mozikr@users.noreply.github.com>
2021-02-24 19:06:33 +0100

    Create movement_AddForce.cs

---
+++ movement_AddForce.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public int movementSpeed;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+            GetComponent<Rigidbody>().AddForce(new Vector3(-movementSpeed, 0, 0));
+        if (Input.GetKey(KeyCode.D))
+            GetComponent<Rigidbody>().AddForce(new Vector3(movementSpeed, 0, 0));
+        if (Input.GetKey(KeyCode.W))
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, movementSpeed));
+        if (Input.GetKey(KeyCode.S))
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, 0, -movementSpeed));
+
+    }
+
+}
